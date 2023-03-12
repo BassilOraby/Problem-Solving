ccca0d290c674974793359d437a23ac656b0df20
Bassil Oraby <38226387+BassilOraby@users.noreply.github.com>
2023-03-12 15:38:13 +0000

    latest update

--- Password/Password/Source.cpp
+++ Password/Password/Source.cpp
@@ -1,31 +1,48 @@
-#include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <string>
-#include <deque>
-using namespace std;
-int n, x;
-deque<string>rule, dict;
-char opts[2] = {'#','0'};
+#include <vector>
 
-void solve() {
-	if (dict.empty())
-		return;
-	else
-	{
-		
-	}
+using namespace std;
+
+const int N = 1e2+5, M = 256+5, OO = 0x3f3f3f3f;
+
+int n, q;
+char dict[N][M], rule[M];
+vector<string> path;
+
+void solve(int i){
+    if(rule[i] == 0){
+        for(string x : path)
+            printf("%s", x.c_str());
+        puts("");
+    }else{
+        if(rule[i] == '0'){ //Digit
+            for(int d = 0 ; d < 10 ; ++d){
+                path.push_back(to_string(d));
+                solve(i+1);
+                path.pop_back();
+            }
+        }else{              //Word
+            for(int w = 0 ; w < n ; ++w){
+                path.push_back(dict[w]);
+                solve(i+1);
+                path.pop_back();
+            }
+        }
+    }
 }
 
-int main() {
-	
-	while (~(scanf_s("%d",&n)))
-	{
-		for (int i = 0; i < n; i++)
-			cin >> dict[i];
-		scanf_s("%d", &x);
-		for (int i = 0; i < x; i++)
-			cin >> rule[i];
-		solve();
-	}
+int main(){
+    while(~scanf("%d", &n)){
+        puts("--");
+        for(int i = 0 ; i < n ; ++i)
+            scanf("%s", dict[i]);
+        scanf("%d", &q);
+        while(q--){
+            scanf("%s", rule);
+            solve(0);
+        }
+    }
+    return 0;
 }
